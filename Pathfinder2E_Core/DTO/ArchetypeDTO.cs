5e646b62ec3f5950db095130e4e057ebe3730328
Joseph <higginsdude@gmail.com>
2022-08-22 19:21:42 +0200

    Added ArchetypeDTO

---
+++ Pathfinder2E_Core/DTO/ArchetypeDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Pathfinder2E_Core.DTO
+{
+    /// <summary>
+    /// DTO for Archetype from api.pathfinder2.fr/v1/pf2/archetype
+    /// </summary>
+    public class ArchetypeDTO
+    {
+        //Map '_id'
+        public string ID { get; set; }
+
+        //Map 'content'
+        public string Content { get; set; }
+
+        //Map 'name'
+        public string Name { get; set; }
+    }
+}
